8130324aa7c62a85f7185cb3d918f0c4894b7474
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2025-03-23 09:46:28 -0700

    Adds missing team_colors.R file
    Defines colors to use when displaying each team in charts

---
+++ team_colors.R
@@ -0,0 +1,12 @@
+# Purpose: Defines the official Unrivaled team colors for use across all visualizations.
+# Rose uses the primary purple, while other teams use shades of grey.
+
+# Define team colors
+TEAM_COLORS <- c(
+  "Rose" = "#9B30FF",        # Bright purple
+  "Lunar Owls" = "#808080",  # Medium grey
+  "Mist" = "#A9A9A9",        # Dark grey
+  "Laces" = "#C0C0C0",       # Silver
+  "Phantom" = "#D3D3D3",     # Light grey
+  "Vinyl" = "#E6E6E6"        # Very light grey
+)
